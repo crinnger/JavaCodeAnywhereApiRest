2dcd3197083268a140d71d209d3c7d96deae60f7
Crinnger <crinnger@gmail.com>
2021-04-27 19:55:59 -0400

    first commit

---
+++ src/main/java/br/com/crinnger/codeanywhere/JavaCodeAnywhereApiRest/JavaCodeAnywhereApiRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.crinnger.codeanywhere.JavaCodeAnywhereApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeAnywhereApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeAnywhereApiRestApplication.class, args);
+	}
+
+}
