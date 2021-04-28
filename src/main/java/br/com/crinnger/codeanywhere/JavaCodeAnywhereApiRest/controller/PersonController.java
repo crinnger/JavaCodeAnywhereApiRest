6ed3d6b951929520d69ebf9c47c55f681776014d
Crinnger <crinnger@gmail.com>
2021-04-27 20:36:04 -0400

    Ajustando doker mysql e deploy no heroku

---
+++ src/main/java/br/com/crinnger/codeanywhere/JavaCodeAnywhereApiRest/controller/PersonController.java
@@ -0,0 +1,10 @@
+package br.com.crinnger.codeanywhere.JavaCodeAnywhereApiRest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+}
---
+++ src/main/java/br/com/crinnger/codeanywhere/JavaCodeAnywhereApiRest/entity/People.java
@@ -0,0 +1,11 @@
+package br.com.crinnger.codeanywhere.JavaCodeAnywhereApiRest.entity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class People {
+
+    @GetMapping
+    public String get(){
+
+    }
+}
